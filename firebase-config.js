6017d2bd17ceadb9841a10c80f67add314ac46ba
Ronie Reyes <reyesronie12@gmail.com>
2025-07-22 13:12:18 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,13 @@
-// firebase-config.js
+// src/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBVhLP24BL4mibJhLuK5H8S4UIyc6SnbkM",
-  authDomain: "nicks-literary-works-29a64.firebaseapp.com",
-  projectId: "nicks-literary-works-29a64",
-  storageBucket: "nicks-literary-works-29a64.firebasestorage.app",
-  messagingSenderId: "1030818110758",
-  appId: "1:1030818110758:web:6a47c5af6d6cba8b9635e7"
+  apiKey: "AIzaSyBVhLP24BL4mibJhLuK5H8S4UIyc6SnbkM",
+  authDomain: "nicks-literary-works-29a64.firebaseapp.com",
+  projectId: "nicks-literary-works-29a64",
+  storageBucket: "nicks-literary-works-29a64.firebasestorage.app",
+  messagingSenderId: "1030818110758",
+  appId: "1:1030818110758:web:6a47c5af6d6cba8b9635e7"
 };
+
+export const app = initializeApp(firebaseConfig);
