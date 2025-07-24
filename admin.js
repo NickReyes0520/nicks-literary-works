267a5c1c0eb610842f4226195a51d40fd4bbf624
Ronie Reyes <reyesronie12@gmail.com>
2025-07-25 00:19:22 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,12 @@
+/* admin.js */
 
+if (username === "nicks-literary-works" && password === "nickreyes0520") {
+  document.querySelectorAll(".admin-only").forEach(btn => btn.style.display = "inline-block");
+  document.getElementById("access-message").style.display = "none";
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("login-success").style.display = "flex"; // Show the success overlay
+
+  setTimeout(() => {
+    document.getElementById("login-success").style.display = "none"; // Hide it after animation
+  }, 3500); // Wait for animation to finish before removing
+}
