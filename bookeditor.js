0c3a35652c8b6196f212d989987d73ec7a888cb7
Ronie Reyes <reyesronie12@gmail.com>
2025-07-26 23:51:09 +0800

    Update bookeditor.js

--- bookeditor.js
+++ bookeditor.js
@@ -1,18 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  // your config here...
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Example: Load books
-async function loadBooks() {
-  const querySnapshot = await getDocs(collection(db, "books"));
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
-    // Dynamically populate your book cards based on this data
-  });
-}
+document.getElementById('fileInput').addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    // TODO: Upload to Firebase or process locally
+    console.log("File selected:", file.name);
+  }
+});
